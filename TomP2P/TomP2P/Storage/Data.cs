973d8ae0b8efb13b9895e2c59ccea17b79ce55e4
Christian Lüthold <biocodr@gmail.com>
2014-10-16 18:20:12 +0200

    start DataMap port

---
+++ TomP2P/TomP2P/Storage/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomP2P.Storage
+{
+     /// <summary>
+     /// This class holds the data for the transport. The data is already serialized and a hash may be created. 
+     /// It is reasonable to create the hash on the remote peer, but not on the local peer. 
+     /// The remote peer uses the hash to tell the other peers, which version is stored and its used quite often.
+     /// </summary>
+     public class Data
+    {
+    }
+}
