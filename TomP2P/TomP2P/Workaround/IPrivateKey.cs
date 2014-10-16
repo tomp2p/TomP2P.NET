f652409884f8cf921b3fdcec90ae3e32a94d5ad0
Christian Lüthold <biocodr@gmail.com>
2014-10-16 10:52:57 +0200

    PKI infrastructure (tmp) workaround

---
+++ TomP2P/TomP2P/Workaround/IPrivateKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomP2P.Workaround
+{
+    /// <summary>
+    /// As there is no .NET equivalent to the java.security.PrivateKey interface,
+    /// this interface serves as a workaround.
+    /// </summary>
+    public interface IPrivateKey
+    {
+    }
+}
---
+++ TomP2P/TomP2P/Workaround/IPublicKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomP2P.Workaround
+{
+    /// <summary>
+    /// As there is no .NET equivalent to the java.security.PublicKey interface,
+    /// this interface serves as a workaround.
+    /// </summary>
+    public interface IPublicKey
+    {
+    }
+}
