2d04f3d3a2d830dcfb2e636b04b2dc1b2eb5055e
Christian Lüthold <biocodr@gmail.com>
2014-10-17 18:37:27 +0200

    SimpleBloomFilter port started

--- TomP2P/TomP2P/Rpc/SimpleBloomFilter.cs
+++ TomP2P/TomP2P/Rpc/SimpleBloomFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace TomP2P.Rpc
 {
-    public class SimpleBloomFilter<T>
+    public class SimpleBloomFilter<T> // TODO implement ISet<T>
     {
+        // TODO serialzation ID required?
+
+        private const int SizeHeaderLength = 2;
+        private const int SizeHeaderElements = 4;
+        private const int SizeHeader = SizeHeaderLength + SizeHeaderElements;
+
+        private readonly int _k;
+        private readonly BitArray _bitArray;
+        private readonly int _byteArraySize;
+        private readonly int _bitArraySize;
+        private readonly int _expectedElements;
     }
 }
