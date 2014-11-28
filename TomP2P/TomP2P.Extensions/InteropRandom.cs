86cc6deac9e7b0670576c968cd37dec13fdebed3
Christian Lüthold <biocodr@gmail.com>
2014-11-28 10:08:10 +0100

    TestInteropSeed

---
+++ TomP2P/TomP2P.Extensions/InteropRandom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TomP2P.Extensions
+{
+    /// <summary>
+    /// A pseudo-random number generator that can be used to create the same output in Java and .NET. The underlying algorithm is inspired from Java's Random implementation.
+    /// The use of this class ensures consistency, whereas Java's Random could be updated anytime.
+    /// </summary>
+    public class InteropRandom
+    {
+
+        private UInt64 _seed;
+
+        public InteropRandom(ulong seed)
+        {
+            _seed = (seed ^ 0x5DEECE66DUL) & ((1UL << 48) - 1);
+        }
+
+        public int NextInt(int n)
+        {
+            if (n <= 0) throw new ArgumentException("n must be positive.");
+
+            if ((n & -n) == n)  // i.e., n is a power of 2
+                return (int)((n * (long)Next(31)) >> 31);
+
+            long bits, val;
+            do
+            {
+                bits = Next(31);
+                val = bits % (UInt32)n;
+            }
+            while (bits - val + (n - 1) < 0);
+
+            return (int)val;
+        }
+
+        protected UInt32 Next(int bits)
+        {
+            _seed = (_seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
+
+            return (UInt32)(_seed >> (48 - bits));
+        }
+    }
+}
---
+++ TomP2P/TomP2P.Tests/Extensions/InteropRandomTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using TomP2P.Extensions;
+
+namespace TomP2P.Tests.Extensions
+{
+    [TestFixture]
+    public class InteropRandomTest
+    {
+        [Test]
+        public void TestSeed()
+        {
+            // create a random seed
+            var r = new Random();
+            var seed = (ulong)r.Next();
+
+            int tests = 20;
+            var randoms = new InteropRandom[tests];
+            var results = new int[tests];
+
+            for (int i = 0; i < tests; i++)
+            {
+                randoms[i] = new InteropRandom(seed);
+                results[i] = randoms[i].NextInt(1000);
+
+                if (i > 0)
+                {
+                    Assert.AreEqual(results[i], results[i - 1]);
+                }
+            }
+        }
+
+        [Test]
+        public void TestInteropSeed()
+        {
+            // use the same seed as in Java
+            const int seed = 1234567890;
+            var random = new InteropRandom(seed);
+            var result1 = random.NextInt(1000);
+            var result2 = random.NextInt(500);
+            var result3 = random.NextInt(10);
+          
+            // requires same results as in Java
+            // result1 is 677
+            // result2 is 242
+            // result3 is 1
+            Assert.AreEqual(result1, 677);
+            Assert.AreEqual(result2, 242);
+            Assert.AreEqual(result3, 1);
+        }
+    }
+}
