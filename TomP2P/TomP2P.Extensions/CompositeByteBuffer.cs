1b71e5b4474d281c7d9472dee4ee4dd58e629e4e
Christian Lüthold <biocodr@gmail.com>
2014-12-01 11:12:36 +0100

    start AlternativeComposityByteBuf port

---
+++ TomP2P/TomP2P.Extensions/CompositeByteBuffer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomP2P.Extensions
+{
+    /// <summary>
+    /// Equivalent of Java TomP2P's AlternativeCompositeByteBuf, which is heavily inspired 
+    /// by Java Netty's CompositeByteBuf, but with a slight different behavior.
+    /// Only the needed parts are ported.
+    /// </summary>
+    public class CompositeByteBuffer
+    {
+    }
+}
