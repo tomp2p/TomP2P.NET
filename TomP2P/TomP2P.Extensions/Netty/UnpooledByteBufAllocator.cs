8ae575c1395a5de49dc549a74938586846943487
Christian Lüthold <biocodr@gmail.com>
2014-12-04 22:35:15 +0100

    UnpooledByteBufAllocator

--- TomP2P/TomP2P.Extensions/Netty/UnpooledByteBufAllocator.cs
+++ TomP2P/TomP2P.Extensions/Netty/UnpooledByteBufAllocator.cs
@@ -1,70 +1,28 @@
-﻿using System;
-
+﻿
 namespace TomP2P.Extensions.Netty
 {
-    public sealed class UnpooledByteBufAllocator : IByteBufAllocator
+    public sealed class UnpooledByteBufAllocator : AbstractByteBufAllocator
     {
         // TODO prefer direct?? needed?
         public static readonly UnpooledByteBufAllocator Default = new UnpooledByteBufAllocator(true);
 
-        private readonly bool _directByDefault;
-        private readonly ByteBuf _emptyBuf;
-
         public UnpooledByteBufAllocator(bool preferDirect)
-        {
-            // from AbstractByteBufAllocator
-            
-        }
+            : base(preferDirect)
+        { }
 
-        public ByteBuf Buffer(int initialCapacity, int maxCapacity)
+        protected override ByteBuf NewDirectBuffer(int initialCapacity, int maxCapacity)
         {
-            if (initialCapacity != 0 || maxCapacity != 0)
-            {
-                throw new ArgumentException("Port does not support calls other than (0, 0).");
-            }
-            return new EmptyByteBuf();
-        }
-
-        // from AbstractByteBufAllocator
-        public ByteBuf DirectBuffer(int initialCapacity)
-        {
-            return DirectBuffer(initialCapacity, Int32.MaxValue);
-        }
-
-        // from AbstractByteBufAllocator
-        public ByteBuf DirectBuffer(int initialCapacity, int maxCapacity)
-        {
-            if (initialCapacity == 0 && maxCapacity == 0)
-            {
-                return new EmptyByteBuf();
-            }
-            Validate(initialCapacity, maxCapacity);
-            return NewDirectBuffer(initialCapacity, maxCapacity);
-        }
-
-        private ByteBuf NewDirectBuffer(int initialCapacity, int maxCapacity)
-        {
-            ByteBuf buf;
             // just return an UnpooledDirectByteBuf
-            buf = new UnpooledDirectByteBuf(this, initialCapacity, maxCapacity);
+            ByteBuf buf = new UnpooledDirectByteBuf(this, initialCapacity, maxCapacity);
+            
+            // TODO toLeakAwareBuffer(buf);
 
-            // TODO toLeadAwareBuffer() needed?
             return buf;
         }
 
-        // from AbstractByteBufAllocator
-        private static void Validate(int initialCapacity, int maxCapacity)
+        protected override ByteBuf NewHeapBuffer(int initialCapacity, int maxCapacity)
         {
-            if (initialCapacity < 0)
-            {
-                throw new ArgumentException("initialCapacity: " + initialCapacity + " (expectd: 0+)");
-            }
-            if (initialCapacity > maxCapacity)
-            {
-                throw new ArgumentException(String.Format(
-                        "initialCapacity: {0} (expected: not greater than maxCapacity({1})",
-                        initialCapacity, maxCapacity));
-            }
+            return new UnpooledHeapByteBuf(this, initialCapacity, maxCapacity);
         }
     }
 }
