40b6fcdd649c1f846a3bec08e7cc6e6bbaa0d2fa
Christian Lüthold <biocodr@gmail.com>
2015-01-07 19:27:08 +0100

    PeerBean partially ported

---
+++ TomP2P/TomP2P/Peers/IBloomfilterFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomP2P.Rpc;
+
+namespace TomP2P.Peers
+{
+    public interface IBloomfilterFactory
+    {
+        SimpleBloomFilter<Number160> CreateContentBloomFilter();
+
+        SimpleBloomFilter<Number160> CreateLocationKeyBloomFilter();
+
+        SimpleBloomFilter<Number160> CreateDomainKeyBloomFilter();
+
+        SimpleBloomFilter<Number160> CreateContentKeyBloomFilter();
+    }
+}
