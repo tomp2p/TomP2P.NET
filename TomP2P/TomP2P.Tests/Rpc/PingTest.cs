adb7f1303ca5ca143cbef7effeecf4aa91f1a4a2
Christian Lüthold <biocodr@gmail.com>
2015-01-14 18:52:30 +0100

    TestPingUdp impl

--- TomP2P/TomP2P.Tests/Rpc/PingTest.cs
+++ TomP2P/TomP2P.Tests/Rpc/PingTest.cs
@@ -24,20 +24,6 @@ namespace TomP2P.Tests.Rpc
 
             try
             {
-                /*
-                 * sender = new PeerBuilder(new Number160("0x9876")).p2pId(55).ports(2424).start();
-            PingRPC handshake = new PingRPC(sender.peerBean(), sender.connectionBean());
-            recv1 = new PeerBuilder(new Number160("0x1234")).p2pId(55).ports(8088).start();
-            new PingRPC(recv1.peerBean(), recv1.connectionBean());
-            FutureChannelCreator fcc = recv1.connectionBean().reservation().create(1, 0);
-            fcc.awaitUninterruptibly();
-            cc = fcc.channelCreator();
-            FutureResponse fr = handshake.pingUDP(recv1.peerAddress(), cc,
-                    new DefaultConnectionConfiguration());
-            fr.awaitUninterruptibly();
-            Assert.assertEquals(true, fr.isSuccess());
-                 * */
-
                 sender = new PeerBuilder(new Number160("0x9876")).
                     SetP2PId(55).
                     SetPorts(2424).
