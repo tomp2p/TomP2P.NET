19f434b5da445a788cb6f9fed2334622113dc28c
Christian Lüthold <biocodr@gmail.com>
2015-02-11 15:02:10 +0100

    ISignatureBuilder

---
+++ TomP2P/TomP2P/P2P/Builder/ShutdownBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomP2P.Connection;
+
+namespace TomP2P.P2P.Builder
+{
+    /// <summary>
+    /// Sets the configuration options for the shutdown command. The shutdown does first a routing, searches
+    /// for its close peers and then sends a quit message so that the other peers know that this peer is offline.
+    /// </summary>
+    public class ShutdownBuilder : DefaultConnectionConfiguration, ISignatureBuilder<ShutdownBuilder>
+    {
+    }
+}
