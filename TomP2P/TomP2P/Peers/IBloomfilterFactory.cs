a93743caebdf7f1bac5962d60bf5649d574ba292
Christian Lüthold <biocodr@gmail.com>
2015-02-11 18:44:37 +0100

    DigestInfo ported

--- TomP2P/TomP2P/Peers/IBloomfilterFactory.cs
+++ TomP2P/TomP2P/Peers/IBloomfilterFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TomP2P.Rpc;
+﻿using TomP2P.Rpc;
 
 namespace TomP2P.Peers
 {
