46861e51e69e76cf1376b526ea45a58db81c2474
Christian Lüthold <biocodr@gmail.com>
2015-02-17 10:41:41 +0100

    intermediate commit

---
+++ TomP2P/TomP2P/Utils/CacheMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomP2P.Utils
+{
+    public class CacheMap<TKey, TValue> : SortedDictionary<TKey, TValue>
+    {
+        private readonly int _maxEntries;
+        private readonly bool _updateEntryOnInsert;
+    }
+}
---
+++ TomP2P/TomP2P/Utils/ConcurrentCacheMap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace TomP2P.Utils
+{
+    public class ConcurrentCacheMap<TKey, TValue> : ConcurrentDictionary<TKey, TValue>
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Number of segments that can be accessed concurrently.
+        /// </summary>
+        public const int SegmentNr = 16;
+        /// <summary>
+        /// Max. number of entries that the map can hold until the least recently used gets replaced.
+        /// </summary>
+        public const int MaxEntries = 1024;
+        /// <summary>
+        /// Time to live for a value. The value may stay longer in the map, but it is considered invalid.
+        /// </summary>
+        public const int DefaultTimeToLive = 60;
+
+    }
+}
