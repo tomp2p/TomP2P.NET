209c832a1bd3df8824224023680c13b6b7299563
Christian Lüthold <biocodr@gmail.com>
2015-02-19 17:28:54 +0100

    BroadcastRpc ported

--- TomP2P/TomP2P/Rpc/BroadcastRpc.cs
+++ TomP2P/TomP2P/Rpc/BroadcastRpc.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using NLog;
 using TomP2P.Connection;
+using TomP2P.Message;
 using TomP2P.P2P;
 using TomP2P.P2P.Builder;
 using TomP2P.Peers;
@@ -15,26 +13,58 @@ namespace TomP2P.Rpc
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        private readonly IBroadcastHandler _broadcastHandler;
+        /// <summary>
+        /// The broadcast handler that is currently used.
+        /// </summary>
+        public IBroadcastHandler BroadcastHandler { get; private set; }
 
         public BroadcastRpc(PeerBean peerBean, ConnectionBean connectionBean, IBroadcastHandler broadcastHandler)
             : base(peerBean, connectionBean)
         {
             Register(Rpc.Commands.Broadcast.GetNr());
-            _broadcastHandler = broadcastHandler;
+            BroadcastHandler = broadcastHandler;
         }
 
         public Task<Message.Message> SendAsync(PeerAddress remotePeer, BroadcastBuilder broadcastBuilder,
             ChannelCreator channelCreator, IConnectionConfiguration configuration)
         {
-            // TODO implement
-            throw new NotImplementedException();
+            var message = CreateRequestMessage(remotePeer, Rpc.Commands.Broadcast.GetNr(),
+                Message.Message.MessageType.RequestFf1);
+            message.SetIntValue(broadcastBuilder.HopCounter);
+            message.SetKey(broadcastBuilder.MessageKey);
+            if (broadcastBuilder.DataMap != null)
+            {
+                message.SetDataMap(new DataMap(broadcastBuilder.DataMap));
+            }
+            var tcsResponse = new TaskCompletionSource<Message.Message>(message);
+            var requestHandler = new RequestHandler(tcsResponse, PeerBean, ConnectionBean, configuration);
+            if (!broadcastBuilder.IsUdp)
+            {
+                return requestHandler.SendTcpAsync(channelCreator);
+            }
+            else
+            {
+                return requestHandler.FireAndForgetUdpAsync(channelCreator);
+            }
         }
 
-        public override void HandleResponse(Message.Message requestMessage, Connection.PeerConnection peerConnection, bool sign, Message.IResponder responder)
+        public override void HandleResponse(Message.Message requestMessage, PeerConnection peerConnection, bool sign, IResponder responder)
         {
-            // TODO implement
-            throw new NotImplementedException();
+            if (!(requestMessage.Type == Message.Message.MessageType.RequestFf1
+                && requestMessage.Command == Rpc.Commands.Broadcast.GetNr()))
+            {
+                throw new ArgumentException("Message content is wrong for this handler.");   
+            }
+            Logger.Debug("Received BROADCAST message: {0}.", requestMessage);
+            BroadcastHandler.Receive(requestMessage);
+            if (requestMessage.IsUdp)
+            {
+                responder.ResponseFireAndForget();
+            }
+            else
+            {
+                responder.Response(CreateResponseMessage(requestMessage, Message.Message.MessageType.Ok));    
+            }
         }
     }
 }
