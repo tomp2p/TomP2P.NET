3b7bea38082b086a4f8cc85c82ac4daffc30b1d0
Christian Lüthold <biocodr@gmail.com>
2015-02-20 17:01:04 +0100

    ISendDirectBuilder ported

--- TomP2P/TomP2P/Rpc/DirectDataRpc.cs
+++ TomP2P/TomP2P/Rpc/DirectDataRpc.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NLog;
 using TomP2P.Connection;
 using TomP2P.Message;
+using TomP2P.Peers;
 
 namespace TomP2P.Rpc
 {
@@ -14,13 +15,15 @@ namespace TomP2P.Rpc
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         private volatile IRawDataReply _rawDataReply;
-        private volatile ObjectDataReply _objectDataReply;
+        private volatile IObjectDataReply _objectDataReply;
 
         public DirectDataRpc(PeerBean peerBean, ConnectionBean connectionBean)
             : base(peerBean, connectionBean)
         {
             Register(Rpc.Commands.DirectData.GetNr());
         }
+
+        public RequestHandler SendInternal(PeerAddress remotePeer, SendDire)
 
         public override void HandleResponse(Message.Message requestMessage, PeerConnection peerConnection, bool sign, IResponder responder)
         {
---
+++ TomP2P/TomP2P/Rpc/ISendDirectBuilder.cs
@@ -0,0 +1,23 @@
+﻿using TomP2P.Connection;
+using TomP2P.Extensions.Workaround;
+using TomP2P.Message;
+
+namespace TomP2P.Rpc
+{
+    public interface ISendDirectBuilder : IConnectionConfiguration
+    {
+        bool IsRaw { get; }
+
+        // TODO ProgressListener needed?
+
+        bool IsSign { get; }
+
+        bool IsStreaming { get; }
+
+        Buffer Buffer { get; }
+
+        object Object { get; }
+
+        KeyPair KeyPair { get; }
+    }
+}
