2661bc92cc35433f88ba2915ca4ebc832afeaa5d
Christian Lüthold <biocodr@gmail.com>
2015-02-21 11:46:49 +0100

    DefaultPeerFilter ported

---
+++ TomP2P/TomP2P/Peers/DefaultPeerFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TomP2P.Peers
+{
+    /// <summary>
+    /// The default peer filter accepts all peers.
+    /// </summary>
+    public class DefaultPeerFilter : IPeerFilter
+    {
+        public bool Reject(PeerAddress peerAddress, ICollection<PeerAddress> all, Number160 target)
+        {
+            // by default, don't reject anything
+            return false;
+        }
+    }
+}
