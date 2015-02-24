39bd9dc4906642b7119b96623cf490902df1d72e
Christian Lüthold <biocodr@gmail.com>
2015-02-24 13:50:17 +0100

    IRequestConfiguration ported

---
+++ TomP2P/TomP2P/P2P/IRequestConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace TomP2P.P2P
+{
+    public interface IRequestConfiguration
+    {
+        int Parallel { get; }
+
+        bool IsForceUdp { get; }
+
+        bool IsForceTcp { get; }
+    }
+}
