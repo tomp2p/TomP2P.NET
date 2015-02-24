d5bc1da86c634e1351283938f9ecf6c690557051
Christian Lüthold <biocodr@gmail.com>
2015-02-24 15:05:40 +0100

    PeerStatRoutingFilter ported

---
+++ TomP2P/TomP2P/Peers/PeerStatRoutingFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TomP2P.P2P;
+
+namespace TomP2P.Peers
+{
+    /// <summary>
+    /// The default filter accepts all peers.
+    /// </summary>
+    public class PeerStatRoutingFilter : IPeerFilter
+    {
+        //private readonly Statistics _statistics;
+        //private readonly int _replicationRate;
+
+        public PeerStatRoutingFilter(Statistics statistics, int replicationRate)
+        {
+            //_statistics = statistics;
+            //_replicationRate = replicationRate;
+        }
+
+        public bool Reject(PeerAddress peerAddress, ICollection<PeerAddress> all, Number160 target)
+        {
+            // TODO in Java: to be implemented
+            return false;
+        }
+    }
+}
