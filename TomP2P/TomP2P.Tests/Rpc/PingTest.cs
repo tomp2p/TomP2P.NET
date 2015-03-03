5e4eeb102382db381f2dc5ebb6f011f078e47b90
Christian Lüthold <biocodr@gmail.com>
2015-03-03 17:42:27 +0100

    TestPingTcp

--- TomP2P/TomP2P.Tests/Rpc/PingTest.cs
+++ TomP2P/TomP2P.Tests/Rpc/PingTest.cs
@@ -11,6 +11,52 @@ namespace TomP2P.Tests.Rpc
     public class PingTest
     {
         [Test]
+        public async void TestPingTcp()
+        {
+            Peer sender = null;
+            Peer recv1 = null;
+            ChannelCreator cc = null;
+            try
+            {
+                sender = new PeerBuilder(new Number160("0x9876"))
+                    .SetMaintenanceTask(Utils2.CreateInfiniteIntervalMaintenanceTask())
+                    .SetChannelServerConfiguration(Utils2.CreateInfiniteTimeoutChannelServerConfiguration(2424, 2424))
+                    .SetP2PId(55)
+                    .SetPorts(2424)
+                    .Start();
+                recv1 = new PeerBuilder(new Number160("0x1234"))
+                    .SetMaintenanceTask(Utils2.CreateInfiniteIntervalMaintenanceTask())
+                    .SetChannelServerConfiguration(Utils2.CreateInfiniteTimeoutChannelServerConfiguration(8088, 8088))
+                    .SetP2PId(55)
+                    .SetPorts(8088)
+                    .Start();
+
+                cc = await recv1.ConnectionBean.Reservation.CreateAsync(0, 1);
+                var tr = sender.PingRpc.PingTcpAsync(recv1.PeerAddress, cc, new DefaultConnectionConfiguration());
+                await tr;
+
+                Assert.IsTrue(!tr.IsFaulted);
+            }
+            finally
+            {
+                if (cc != null)
+                {
+                    cc.ShutdownAsync().Wait();
+                }
+                if (sender != null)
+                {
+                    sender.ShutdownAsync().Wait();
+                }
+                if (recv1 != null)
+                {
+                    recv1.ShutdownAsync().Wait();
+                }
+            }
+        }
+
+
+
+        [Test]
         public async void TestPingUdp()
         {
             Peer sender = null;
