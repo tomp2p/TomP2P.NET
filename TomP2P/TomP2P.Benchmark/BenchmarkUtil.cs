8ef8d7a084fc63f461f4b6a61ee440244fa3b621
Christian Lüthold <biocodr@gmail.com>
2015-03-12 15:38:20 +0100

    added millisecond printout

--- TomP2P/TomP2P.Benchmark/BenchmarkUtil.cs
+++ TomP2P/TomP2P.Benchmark/BenchmarkUtil.cs
@@ -66,12 +66,17 @@ namespace TomP2P.Benchmark
         {
             watch.Stop();
             Console.WriteLine("{0}: Stopped Benchmarking.", caller);
-            Console.WriteLine("{0}: {1:0.000} ns ({2:0.000} s)", caller, watch.ToNanos(), watch.ToSeconds());
+            Console.WriteLine("{0}: {1:0.000} ns | {2:0.000} ms | {3:0.000} s)", caller, watch.ToNanos(), watch.ToMillis(), watch.ToSeconds());
         }
 
         private static double ToSeconds(this Stopwatch watch)
         {
             return watch.ElapsedTicks / (double)Stopwatch.Frequency;
+        }
+
+        private static double ToMillis(this Stopwatch watch)
+        {
+            return watch.ToSeconds() * 1000;
         }
 
         private static double ToNanos(this Stopwatch watch)
