07fd5df900ef0206135cd2c8ade13dab9a27c0f4
Christian Lüthold <biocodr@gmail.com>
2015-04-26 18:39:01 +0200

    IPeerMapFilter ported

---
+++ TomP2P/TomP2P.Core/Peers/IPeerMapFilter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace TomP2P.Core.Peers
+{
+    /// <summary>
+    /// A filter that can prevent peers from being stored in the map.
+    /// Therefore, it will be ignored in the routing.
+    /// </summary>
+    public interface IPeerMapFilter
+    {
+        /// <summary>
+        /// Each peer that is added in the map runs through this filter.
+        /// </summary>
+        /// <param name="peerAddress">The peer address that is going to be added to the map.</param>
+        /// <param name="peerMap">The peer map where additional information can be retrieved.</param>
+        /// <returns>True, if the peer address should not be added, false otherwise.</returns>
+        bool RejectPeerMap(PeerAddress peerAddress, PeerMap peerMap);
+
+        bool RejectPreRouting(PeerAddress peerAddress, IEnumerable<PeerAddress> all);
+    }
+}
--- TomP2P/TomP2P.Dht/DhtBuilder.cs
+++ TomP2P/TomP2P.Dht/DhtBuilder.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TomP2P.Core.Connection;
+using TomP2P.Core.P2P;
 using TomP2P.Core.P2P.Builder;
+using TomP2P.Core.Peers;
+using TomP2P.Extensions.Workaround;
 
 namespace TomP2P.Dht
 {
+    /// <summary>
+    /// Every DHT builder has those methods in common.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class DhtBuilder<T> : DefaultConnectionConfiguration, IBasicBuilder<T>, IConnectionConfiguration, ISignatureBuilder<T> where T : DhtBuilder<T>
     {
         protected readonly PeerDht _peer;
+        protected readonly Number160 _locationKey;
+        protected Number160 _domainKey;
+        protected Number160 _versionKey;
+
+        protected RoutingConfiguration _routingConfiguration;
+        protected RequestP2PConfiguration _requestP2PConfiguration;
+        protected TaskCompletionSource<ChannelCreator> _tcsChannelCreator;
+
+        private bool _protectDomain;
+        private KeyPair _keyPair;
+        private bool _streaming;
+
+        private IEnumerable<Peermapf> 
     }
 }
