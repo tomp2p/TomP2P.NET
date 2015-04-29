841c447cd479b9bf7b9b00f7eacf53735aeb3ff6
Christian Lüthold <biocodr@gmail.com>
2015-04-29 12:03:07 +0200

    IReplicationListener ported

---
+++ TomP2P/TomP2P.Dht/IReplicationListener.cs
@@ -0,0 +1,11 @@
+﻿using TomP2P.Core.Peers;
+
+namespace TomP2P.Dht
+{
+    public interface IReplicationListener
+    {
+        void DataInserted(Number160 locationKey);
+
+        void DataRemoved(Number160 locationKey);
+    }
+}
