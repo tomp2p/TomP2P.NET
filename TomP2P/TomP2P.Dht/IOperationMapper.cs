40d59b35108b989b1624a1e2a7f22d647b65fcba
Christian Lüthold <biocodr@gmail.com>
2015-04-29 12:13:38 +0200

    IOperationMapper ported

---
+++ TomP2P/TomP2P.Dht/IOperationMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using TomP2P.Core.Connection;
+using TomP2P.Core.Message;
+using TomP2P.Core.Peers;
+
+namespace TomP2P.Dht
+{
+    /// <summary>
+    /// The operations that create many RPCs.
+    /// </summary>
+    /// <typeparam name="T">The type of the task that takes care of all the RPC tasks.</typeparam>
+    public interface IOperationMapper<in T> where T : TcsDht
+    {
+        /// <summary>
+        /// Creates a single RPC.
+        /// </summary>
+        /// <param name="channelCreator">The channel creator to create an UDP or TCP channel.</param>
+        /// <param name="remotePeerAddress">The address of the remote peer.</param>
+        /// <returns></returns>
+        Task<Message> Create(ChannelCreator channelCreator, PeerAddress remotePeerAddress);
+
+        /// <summary>
+        /// Called when the responses' overall task is finished.
+        /// </summary>
+        /// <param name="task">The overall task.</param>
+        /// <param name="tasksCompleted"></param>
+        void Response(T task, Task tasksCompleted);
+
+        /// <summary>
+        /// Called whenever a single task is finished.
+        /// </summary>
+        /// <param name="taskResponse"></param>
+        void IntermediateResponse(Task<Message> taskResponse);
+    }
+}
